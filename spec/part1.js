ebe6e817dddd1f8e05a3ca248a39baf6ce6689da
Tessa Thornberry <tthornberryclass@gmail.com>
2022-09-07 12:28:37 -0700

    correct and pass tests for underbar.js through _.reject - pair with Hieu Ngo

--- spec/part1.js
+++ spec/part1.js
@@ -126,7 +126,7 @@
           iterations.push([letter, index]);
         });
 
-        expect(iterations).to.eql(FILL_ME_IN);
+        expect(iterations).to.eql([[ 'a', 0 ], [ 'b', 1 ], [ 'c', 2 ]]);
       });
 
       it('should iterate over arrays and provide access to the original collection', function() {
@@ -153,7 +153,7 @@
           iterations.push(letter);
         });
 
-        expect(iterations).to.not.include(FILL_ME_IN);
+        expect(iterations).to.not.include(letters.someProperty);
       });
 
       it('should iterate over objects and provide access to each value', function() {
@@ -219,7 +219,7 @@
       it('should find 40 in the list', function() {
         var numbers = [10, 20, 30, 40, 50];
 
-        expect(_.indexOf(FILL_ME_IN, 40)).to.equal(3);
+        expect(_.indexOf(numbers, 40)).to.equal(3);
       });
 
       it('should be able to compute indexOf even when the native function is undefined', function() {
@@ -229,14 +229,14 @@
       });
 
       it('returns -1 when the target cannot be found not in the list', function() {
-        var numbers = FILL_ME_IN;
+        var numbers = [1, 2, 3, 4];
 
         expect(_.indexOf(numbers, 35)).to.equal(-1);
       });
 
       it('returns the first index that the target can be found at when there are multiple matches', function() {
-        var numbers = FILL_ME_IN;
-        expect(FILL_ME_IN).to.equal(1);
+        var numbers = [1, 2, 20, 4, 2, 15];
+        expect(_.indexOf(numbers, 2)).to.equal(1);
       });
     });
 
@@ -251,7 +251,7 @@
 
       it('should return all odd numbers in an array', function() {
         var isOdd = function(num) { return num % 2 !== 0; };
-        var odds = FILL_ME_IN;
+        var odds = _.filter([1, 2, 3, 4, 5, 6], isOdd);
 
         expect(odds).to.eql([1, 3, 5]);
       });
@@ -332,7 +332,7 @@
         var iterator = function(value) { return value === 1; };
         var numbers = [1, 2, 2, 3, 4, 4];
 
-        expect(_.uniq(FILL_ME_IN)).to.eql([1, 2]);
+        expect(_.uniq(numbers)).to.eql([1, 2]);
       });
 
       it('should produce a brand new array instead of modifying the input array', function() {
--- src/underbar.js
+++ src/underbar.js
@@ -52,6 +52,15 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -73,16 +82,46 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var array = [];
+
+    _.each(collection, function(item) {
+      if (test(item)) {
+        array.push(item);
+      }
+    });
+
+    return array;
   };
 
   // Return all elements of an array that don't pass a truth test.
+  // TIP: see if you can re-use _.filter() here, without simply
+  // copying code in and modifying it
   _.reject = function(collection, test) {
-    // TIP: see if you can re-use _.filter() here, without simply
-    // copying code in and modifying it
+    var array = [];
+
+    _.filter(collection, function(item) {
+      if (!test(item)) {
+        array.push(item);
+      }
+    });
+
+    return array;
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
+    var uniqueArray = [];
+
+    if (!iterator) {
+      iterator = _.identity;
+    }
+    _.each(array, function(item) {
+      if (uniqueArray.includes(item) === false) {
+        uniqueArray.push(item);
+      }
+    });
+
+    return uniqueArray;
   };
 
 
